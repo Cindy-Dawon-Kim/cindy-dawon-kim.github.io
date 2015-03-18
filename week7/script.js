211c2f80e3f3f701b28f0fed0895710377a2e7ba
Cindy-Dawon-Kim <kimd655@newschool.edu>
2015-03-17 20:49:14 -0400

    week7

---
+++ week7/script.js
@@ -0,0 +1,10 @@
+var name = prompt("What is your name?");
+document.getElementById("xholder").innerHTML = name;
+
+var birthday = prompt("When is your birthday?");
+document.getElementById("xbday").innerHTML = birthday;
+
+var color = prompt("What is your favorite color?");
+document.getElementById("xcolor").innerHTML = color;
+document.getElementById("xcolor").style.color=color;xbday.style.color=color;
+document.getElementById("xcolor").style.color=color;xholder.style.color=color;
