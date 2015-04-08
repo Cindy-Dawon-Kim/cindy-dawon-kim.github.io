006f62440d016c02f11614229bd10750c1c93518
Cindy-Dawon-Kim <kimd655@newschool.edu>
2015-04-07 21:18:24 -0400

    js2

--- js2/script.js
+++ js2/script.js
@@ -4,20 +4,20 @@ window.onload = function() {
 
 	link.onclick = function() {
 
-		var adjective1 = prompt("Choose an adjective!");
-		document.getElementById("word-a").innerHTML = adjective1 + "from the";
+		var adjective1 = prompt("Choose an adjective ending with -ing!");
+		document.getElementById("word_a").innerHTML = adjective1 + " from the ";
 
 		var place1 = prompt("Choose a place!");
-		document.getElementById("word-b").innerHTML = place1;
+		document.getElementById("word_b").innerHTML = place1;
 
-		var noun-plural = prompt("Choose a plural noun!");
-		document.getElementById("word-c").innerHTML = "Fallen" + noun-plural;
+		var noun_plural = prompt("Choose a plural noun!");
+		document.getElementById("word_c").innerHTML = "Fallen " + noun_plural;
 
 		var adjective2 = prompt("Choose another adjective!");
-		document.getElementById("word-d").innerHTML = adjective2;
+		document.getElementById("word_d").innerHTML = adjective2;
 
 		var place2 = prompt("Choose another place!");
-		document.getElementById("word-e").innerHTML = "at the" + place2;
+		document.getElementById("word_e").innerHTML = " at the " + place2;
 
 	};
 
