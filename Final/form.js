69194fd016fa55eaebc7f3da5d0e57c3663a212d
Cindy-Dawon-Kim <kimd655@newschool.edu>
2015-05-11 15:05:21 -0400

    final

---
+++ Final/form.js
@@ -0,0 +1,24 @@
+$(function() {
+
+  function requestFile(url){
+    $ajax({
+      url: url,
+      cache: false,
+      dataType:"json",
+      success: function(data){
+        $("#return").html(
+          "Thank you for your subscription " + data["email"] + "!"
+          );
+      },
+      error: function() {
+        window.alert("Please fill in the requested fields!")
+      }
+
+    })
+  }
+
+  $("#ajaxrequest").click(function() {
+    requestFile("data.json");
+  });
+
+});
